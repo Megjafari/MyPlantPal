6cd76793911f11e1e5222f580f4d886561778352
Megjafari <megberserker@gmail.com>
2025-11-13 20:26:45 +0100

    Added plant and plantservice classes and some predefined plant templates

---
+++ MyPlantPal/Models/Plant.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPlantPal.Models
+{
+    public class Plant
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public string OwnerUsername { get; set; }
+        public DateTime LastWatered { get; set; }
+        public int WateringIntervalDays { get; set; }
+
+        // Calculated properties
+        public DateTime NextWateringDate => LastWatered.AddDays(WateringIntervalDays);
+        public bool NeedsWatering => DateTime.Now >= NextWateringDate;
+
+        // Constructor for JSON deserialization
+        public Plant()
+        {
+            Id = Guid.NewGuid().ToString();     // Generate a new unique identifier
+            LastWatered = DateTime.Now;
+            WateringIntervalDays = 7; // Default value
+        }
+
+        // Constructor for creating new plants
+        public Plant(string name, string species, string ownerUsername) : this()
+        {
+            Name = name;
+            Species = species;
+            OwnerUsername = ownerUsername;
+        }
+    }
+}
